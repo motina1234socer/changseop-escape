9a5200e6bf9af549238f6162644230591d62b01d
motina1234socer <135096330+motina1234socer@users.noreply.github.com>
2025-03-23 13:32:52 +0900

    sibal jot gat ta

---
+++ Assets/itemusing.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemusing : MonoBehaviour
+{
+        public bool flashing = false;
+    public GameObject flashlight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q)) {
+            if(flashing) {
+                flashing = false;
+            } else {
+                flashing = true;
+            }
+        }
+        flash_onoff(flashing);
+    }
+
+    private void flash_onoff(bool a) {
+        flashlight.SetActive(a);
+    }
+}
